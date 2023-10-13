b4219d503555ee2eb2f20f7908a2dde4fa6688bd
Kyungryul lee <leekyung1111@gmail.com>
2023-10-13 15:05:14 +0900

    fix : 덧셈, 뺄셈 테스트 코드 작성
    - 덧셈, 뺄셈 테스트 코드 작성
    
    Ref #001

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calculator {
+    public static int calculate(int operand1, String operator, int operand2 ) {
+        if (operator.equals("+")) {
+            return operand1 + operand2;
+        } else if (operator.equals("-")) {
+            return operand1 - operand2;
+        }
+        return 0;
+    }
+}
